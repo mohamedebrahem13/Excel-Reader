04d935ec702274306dcb69fe9efcbf64ce5ff217
Mohamed Ebrahim <mo.ebrahim@posmission.com>
2025-01-12 14:39:00 +0200

    add fonts for ar and en

--- app/src/main/java/com/example/excel_reader/ui/theme/Theme.kt
+++ app/src/main/java/com/example/excel_reader/ui/theme/Theme.kt
@@ -106,7 +106,7 @@ fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
   val colorScheme = when {
       dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -120,7 +120,7 @@ fun AppTheme(
 
   MaterialTheme(
     colorScheme = colorScheme,
-    typography = Typography,
+    typography = dynamicTypography(),
     content = content
   )
 }
--- app/src/main/java/com/example/excel_reader/ui/theme/Type.kt
+++ app/src/main/java/com/example/excel_reader/ui/theme/Type.kt
@@ -1,34 +1,74 @@
 package com.example.excel_reader.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.sp
+import com.example.excel_reader.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+// Bahij Font Family
+val BahijFontFamily = FontFamily(
+    Font(R.font.bahij_black, FontWeight.Black),
+    Font(R.font.bahij_bold, FontWeight.Bold),
+    Font(R.font.bahij_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.bahij_extra_light, FontWeight.ExtraLight),
+    Font(R.font.bahij_plain, FontWeight.Normal),
+    Font(R.font.bahij_semi_bold, FontWeight.SemiBold),
+    Font(R.font.bahij_semi_light, FontWeight.Light)
 )
+
+val GalanoFontFamily = FontFamily(
+    Font(R.font.galano_grotesque_black, FontWeight.Black),
+    Font(R.font.galano_grotesque_bold, FontWeight.Bold),
+    Font(R.font.galano_grotesque_heavy, FontWeight.ExtraBold),
+    Font(R.font.galano_grotesque_medium, FontWeight.Medium),
+    Font(R.font.galano_grotesque_regular, FontWeight.Normal),
+    Font(R.font.galano_grotesque_semibold, FontWeight.SemiBold),
+    Font(R.font.galano_grotesque_thin, FontWeight.Thin)
+)
+// Helper Function to Get Font Family
+@Composable
+fun getFontFamily(): FontFamily {
+    val layoutDirection = LocalLayoutDirection.current
+    return if (layoutDirection == LayoutDirection.Rtl) BahijFontFamily else GalanoFontFamily
+}
+
+// Custom Typography
+@Composable
+fun dynamicTypography(): Typography {
+    val fontFamily = getFontFamily() // Call the composable function directly
+
+    // Define typography with the selected font family
+    return Typography(
+        displayLarge = TextStyle(
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Black,
+            fontSize = 30.sp
+        ),
+        headlineMedium = TextStyle(
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        ),
+        bodyLarge = TextStyle(
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp
+        ),
+        bodyMedium = TextStyle(
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 14.sp
+        ),
+        labelSmall = TextStyle(
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Light,
+            fontSize = 12.sp
+        )
+    )
+}
